d713cddf9f71468cbbe674c509f596f392f3932c
ZenIsBestWolf <ZenIsBestWolf@gmail.com>
2017-09-26 16:42:11 -0400

    add !invite
    now you can bring maddie to a server near you <3

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('message', message => {
   if (message.content === prefix + 'help') {
     message.delete()
     message.reply('check your DMs!');
-    message.author.send('Here\'s the list of commands!\n ```\n!ripchat - (Formally !ripchatimg) Sends an image grieving over the chat, from the TwoKinds Group Chat discord server.\n!blanket - If someone\'s cold, not anymore!\n!help - Display\'s THIS message.\n!triforce - The triforce of bed!\n!coffee - When someone\'s hyper.\n!triggered - If someone says \"Alot\" or any other things that trigger you, use this.\n!mocha - The saviour of lack of sleep and replenisher of energy!\n!source - Links you to the GitHub where Maddie\'s code is, since she\'s open source.\n!angrykeith - A picture of the character Keith, from TwoKinds, very angry!\n!okevals - The good ol\' OK from Evals!\n!plottwister - Hey, watch it! That\'s my exposition!\n!beaned - friccin beaned kiddo.\n!blush - Oh the flattery!\n!smug - For when you took that extra cookie from the jar.\n!boi - U WAT BOI!?\n!shook - Similar meaning to !triggered, but less intense.\n!embarrassedmike - Whoopsies!\n!embarrassedkatvals - the same as !embarrassedmike but with a different person.\n!really - Just really?\n!excusemewat - bruh watchu say???\n!saywatnow - Similar to !excusemewat.\n!brainfart - Quick! I need some brain-fart laxitive!\n!rekt LOL XD FREAKING REKT KIDDO\n!anyonethere - Hello. It\'s me. I was wonderin if anyone was here after all these minutes.\n!embarrassedkeith - same as !embarrassedmike, just a different character.\n!smug2 - Similar to !smug.\n!satisfaction - When you\'re all too satisfied.\n!profanity - Send this to those who swear too much.```More coming!');
+    message.author.send('Here\'s the list of commands!\n ```\n!ripchat - (Formally !ripchatimg) Sends an image grieving over the chat, from the TwoKinds Group Chat discord server.\n!blanket - If someone\'s cold, not anymore!\n!help - Display\'s THIS message.\n!triforce - The triforce of bed!\n!coffee - When someone\'s hyper.\n!triggered - If someone says \"Alot\" or any other things that trigger you, use this.\n!mocha - The saviour of lack of sleep and replenisher of energy!\n!source - Links you to the GitHub where Maddie\'s code is, since she\'s open source.\n!angrykeith - A picture of the character Keith, from TwoKinds, very angry!\n!okevals - The good ol\' OK from Evals!\n!plottwister - Hey, watch it! That\'s my exposition!\n!beaned - friccin beaned kiddo.\n!blush - Oh the flattery!\n!smug - For when you took that extra cookie from the jar.\n!boi - U WAT BOI!?\n!shook - Similar meaning to !triggered, but less intense.\n!embarrassedmike - Whoopsies!\n!embarrassedkatvals - the same as !embarrassedmike but with a different person.\n!really - Just really?\n!excusemewat - bruh watchu say???\n!saywatnow - Similar to !excusemewat.\n!brainfart - Quick! I need some brain-fart laxitive!\n!rekt LOL XD FREAKING REKT KIDDO\n!anyonethere - Hello. It\'s me. I was wonderin if anyone was here after all these minutes.\n!embarrassedkeith - same as !embarrassedmike, just a different character.\n!smug2 - Similar to !smug.\n!satisfaction - When you\'re all too satisfied.\n!profanity - Send this to those who swear too much.\n!invite - DMs you an invite link to add me to your server!```More coming!');
     console.log(message.author.tag + ' used ' + message.content);
   }
   if (message.content === prefix + 'triforce') {
@@ -150,6 +150,11 @@ client.on('message', message => {
     message.delete()
       message.channel.send('https://prntscr.com/fbcth1');
       console.log(message.author.tag + ' used ' + message.content);
+  }
+  if (message.content === prefix + 'invite') {
+	  message.delete()
+	  message.channel.reply(' check your DMs for an invite link!')
+	  message.author.send('If you want to get ME in YOUR server, go HERE: https://discordapp.com/oauth2/authorize?client_id=362327458554445865&scope=bot&permissions=125952\nPlease don\'t change the perms, I need these to run properly!')
   }
 });
 
