1ddce6604e7e6fdabecd504188674bb9aa53afb2
Zen <zenisbestwolf@gmail.com>
2017-10-09 07:49:15 -0400

    minor variable thing (better nexy?)

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ client.on('ready' ,() => {
 });
 process.on('unhandledRejection', console.error);
 var prefix = '!';
+var zen = "183672121522782208"
 
 client.on('message', message => {
   var unfilteredcommand = message.content.substring(prefix.length).split(' ')
@@ -17,20 +18,20 @@ client.on('message', message => {
   if (command === 'help') {
     message.delete()
     message.author.send('Here\'s the list of commands!\n ```\n!ripchat - Sends an image grieving over the chat, from the TwoKinds Group Chat discord server.\n!blanket - If someone\'s cold, not anymore!\n!help - Display\'s THIS message.\n!triforce - The triforce of bed!\n!coffee - When someone\'s hyper.\n!triggered - If someone says \"Alot\" or any other things that trigger you, use this.\n!source - Links you to the GitHub where Maddie\'s code is, since she\'s open source.\n!angrykeith - A picture of the character Keith, from TwoKinds, very angry!\n!okevals - The good ol\' OK from Evals!\n!plottwister - Hey, watch it! That\'s my exposition!\n!beaned - friccin beaned kiddo.\n!smug - For when you took that extra cookie from the jar.\n!boi - U WAT BOI!?\n!shook - Similar meaning to !triggered, but less intense.\n!embarrassedmike - Whoopsies!\n!embarrassedkat - the same as !embarrassedmike but with a different person.\n!really - Just really?\n!derp - herpa derp\n!rekt LOL XD FREAKING REKT KIDDO\n!wat - wait wat\n!embarrassedkeith - same as !embarrassedmike, just a different character.\n!smug2 - Similar to !smug.\n!profanity - Send this to those who swear too much.\n!invite - DMs you an invite link to add me to your server!\n!ches - no one!!1!\n!superhappy - HOORAY YAY OMG YAS\n!testserver  - Recieve an invite to the testing server, ask an admin for perms!\n!jerks - meanies >:(\n!kek - K E K\n!shhh - (3 h\'s) SHUTUP but subtle.\n!topkek - pretty much !kek but its not.\n!call - for all types of calls!```DM ZenIsBestWolf#0446 with all of your suggestions!');
-    if (message.author.id === "183672121522782208") {
+    if (message.author.id === zen) {
       message.author.send("Hi Zen!\nAdministrator Commands: ```" + prefix + "shutdown - Shutdown Maddie.\n" + prefix + "chat [channel] [message] - Chats the message as this bot, in the specified channel. BE CAREFUL! Invalid channel WILL break Maddie.\n" + prefix + "log [message] - Logs the message into the logs.```")
     };
     return;
   };
   if (command === 'shutdown') {
-    if (message.author.id === "183672121522782208") {
+    if (message.author.id === zen) {
     process.exit();
     return;
   }
   return;
 };
   if (command === 'chat') {
-    if (message.author.id === "183672121522782208") {
+    if (message.author.id === zen) {
     message.delete();
     var chantar = unfilteredcommand[1]
     var chtmsg = unfilteredcommand.join(' ').slice(chantar.length + 5)
@@ -40,7 +41,7 @@ client.on('message', message => {
   return;
 };
   if (command === 'log') {
-    if (message.author.id === "183672121522782208") {
+    if (message.author.id === zen) {
     message.delete();
     console.log(args);
   return;
